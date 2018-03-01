2f9fcecd7b00315cb01558d3391c9cd2ceae9155
michelamar <mmarchini@stuy.edu>
2018-03-01 14:48:56 -0500

    classwork

--- svg.js
+++ svg.js
@@ -2,28 +2,37 @@ var svg = document.getElementById("svjesus");
 
 var b1 = document.getElementById("clear");
 
-var clear = function(){
-    clear = document.createElementNS("http://www.w3.org/2000/sg", "rect");
-    clear.setAttribute(x, 0);
-    clear.setAttribute(y, 0);
-    clear.setAttribute(width, 500);
-    clear.setAttribute(height, 500);
-    clear.setAttribute(fill, "white");
-    svg.appendChild(clear);
+var x = -1;
+var y = -1;
+
+var clear_screen = function(){
+    while (svg.lastChild) {
+    	svg.removeChild(svg.lastChild);
+    }
 };
 
 var make_circle = function(e){
     circle = document.createElementNS("http://www.w3.org/2000/sg", "circle");
-
+    circle.setAttribute("cx", e.offsetX);
+    circle.setAttribute("cy", e.offsetY);
+    circle.setAttribute("r", "30");
+    svg.appendChild(circle);
 };
 
 
 var make_shape = function(e){
-    ctx.lineTo(e.offsetX, e.offsetY);
-    ctx.stroke();
+    if (x != -1 && y != -1){
+    	line = document.createElementNS("http://www.w3.org/2000/sg", "line");
+    	line.setAttribute("x1", x);
+    	line.setAttribute("y1", y);
+	line.setAttribute("x2", e.offsetX);
+	line.setAttribute("y2", e.offsetY);
+    }
+    x = e.offsetX;
+    y = e.offsetY;
     make_circle(e)
 };
 
 
-b1.addEventListener("click", clear);
-c.addEventListener("click", make_shape);
+b1.addEventListener("click", clear_screen);
+svg.addEventListener("click", make_shape);

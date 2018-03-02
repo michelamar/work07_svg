a1abc28cdfca8c9764ca5e0bd9813170db3621ef
Michela Marchini <mmarchini@stuy.edu>
2018-03-01 22:10:00 -0500

    wow i had a typo in the namespace ihml

--- svg.js
+++ svg.js
@@ -12,7 +12,7 @@ var clear_screen = function(){
 };
 
 var make_circle = function(e){
-    circle = document.createElementNS("http://www.w3.org/2000/sg", "circle");
+    circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
     circle.setAttribute("cx", e.offsetX);
     circle.setAttribute("cy", e.offsetY);
     circle.setAttribute("r", "30");
@@ -24,11 +24,13 @@ var make_circle = function(e){
 
 var make_shape = function(e){
     if (prevy){
-    	line = document.createElementNS("http://www.w3.org/2000/sg", "line");
+    	line = document.createElementNS("http://www.w3.org/2000/svg", "line");
     	line.setAttribute("x1", prevx);
     	line.setAttribute("y1", prevy);
 	line.setAttribute("x2", e.offsetX);
 	line.setAttribute("y2", e.offsetY);
+	line.setAttribute("stroke", "blue");
+	svg.appendChild(line);
     }
     prevx = e.offsetX;
     prevy = e.offsetY;
@@ -37,4 +39,4 @@ var make_shape = function(e){
 
 
 b1.addEventListener("click", clear_screen);
-svg.addEventListener("click", make_circle);
+svg.addEventListener("click", make_shape);
